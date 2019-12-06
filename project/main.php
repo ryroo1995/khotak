352f0340d292810d5521f8b6ad12f373aaf42900
ryroo1995 <m.almathlouthi@gmail.com>
2019-12-06 09:01:10 +0300

    just the vew of the code

--- project/main.php
+++ project/main.php
@@ -5,128 +5,132 @@ include('includes/header.php');
 
 
 <!-------------------------------------------------------------------------------------------------------------------------------------->
-    <div class="content-section">
-        <div class="overflow"></div>
-        <div class="container">
-      <div class="con">
-    <div class="wel" id="well-s">
-       welcome to KHOTAK website
-    </div> <!-- end of welcom -->
-                                         <span class="arrow ">
-                 <i class="fa fa-chevron-down hvr-icon-hang typed-cursor"></i>
-                                 </span>
-            </div><!--end con-->
-       </div><!-----------------end container-->
+<div class="content-section">
+    <div class="overflow"></div>
+    <div class="container">
+        <div class="con">
+            <div class="wel" id="well-s">
+                welcome to KHOTAK website
+            </div> <!-- end of welcom -->
+            <span class="arrow ">
+                <i class="fa fa-chevron-down hvr-icon-hang typed-cursor"></i>
+            </span>
+        </div>
+        <!--end con-->
     </div>
+    <!-----------------end container-->
+</div>
 <!-------------------------------------------------------------------------------------------------------------------------------------->
-   <!-- section 1------------------------------------------------------------------------------------------------------------------ -->
-   <section class="section1 text-center">
-       <div class="container">
+<!-- section 1------------------------------------------------------------------------------------------------------------------ -->
+<section class="section1 text-center">
+    <div class="container">
 
-       <h1 id="htext">What is KHOTAK?</h1>
-       <p class="p1">
-Khotak, website is a system to whom been interested for education, especially for graduations from high school  universities and institutions. Although, it can help educational field in many ways such as investment learning, advertising, Science, knowledge and marketing.
-Moreover, it aims to serve the graduated with the technological advances’ revolution, by making it easier for them to find the best specialty without too much efforts and time. This also reduces the effort and time for the educational centers too.
-      </p>
-           <hr>
-       </div>
+        <h1 id="htext">What is KHOTAK?</h1>
+        <p class="p1">
+            Khotak, website is a system to whom been interested for education, especially for graduations from high school universities and institutions. Although, it can help educational field in many ways such as investment learning, advertising, Science, knowledge and marketing.
+            Moreover, it aims to serve the graduated with the technological advances’ revolution, by making it easier for them to find the best specialty without too much efforts and time. This also reduces the effort and time for the educational centers too.
+        </p>
+        <hr>
+    </div>
 
-   </section>
-
-
+</section>
 
 
-   <!-- section 2 ----------------------------------------------------------------------------------------------------------------  -->
-    <section class="section2">
-        <div class="container">
+
+
+<!-- section 2 ----------------------------------------------------------------------------------------------------------------  -->
+<section class="section2">
+    <div class="container">
 
         <div class="row">
 
             <div class="col-md-6">
-              <div class="content-section2">
-                       <h1>specialization</h1>
-        <p>
-Specialization is a method of production whereby an entity focuses on the production of a limited scope of goods to gain a greater degree of efficiency. Many countries, for example, specialize in producing the goods and services that are native to their part of the world, and they trade for other goods and services.
-<br>
-This specialization is thus the basis of global trade, as few countries have enough production capacity to be completely self-sustaining.
-      </p>
-                  </div>
+                <div class="content-section2">
+                    <h1>specialization</h1>
+                    <p>
+                        Specialization is a method of production whereby an entity focuses on the production of a limited scope of goods to gain a greater degree of efficiency. Many countries, for example, specialize in producing the goods and services that are native to their part of the world, and they trade for other goods and services.
+                        <br>
+                        This specialization is thus the basis of global trade, as few countries have enough production capacity to be completely self-sustaining.
+                    </p>
+                </div>
             </div>
 
             <div class="col-md-6">
 
-       <div class="vid2">
-          <iframe width="100%" height="400" src="https://www.youtube.com/embed/xeip3dQqZhU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="">
+                <div class="vid2">
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/xeip3dQqZhU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="">
                     </iframe>
 
-       </div>
+                </div>
             </div>
 
-             </div><!--end row-->
+        </div>
+        <!--end row-->
 
 
 
 
-<hr>
+        <hr>
 
-        </div><!--end contianer-->
+    </div>
+    <!--end contianer-->
 
-    </section>
-
-
-
-    <!-- section 3---------------------------------------------------------------------------------------------------------------- -->
-
-   <section class="section3 text-center">
-       <div class="container">
-           <div class="row">
-           <div class="col-md-12">
-                      <div class="back3">
-           <h1 id="htext">confissins</h1>
-            <p class="p3">
-Specialization is a method of production whereby an entity focuses on the production of a limited scope of goods to gain a greater degree of efficiency. Many countries, for example, specialize in producing the goods and services that are native to their part of the world, and they trade for other goods and services.
-      </p>
-       </div>
-               </div>
-               <div class="col-md-12">
-                       <div class="vid3">
-
-            <iframe width="100%" height="400" src="https://www.youtube.com/embed/HgkWqYDGuFw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
-
-       </div>
-               </div>
-           </div>
+</section>
 
 
 
+<!-- section 3---------------------------------------------------------------------------------------------------------------- -->
 
+<section class="section3 text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="back3">
+                    <h1 id="htext">confissins</h1>
+                    <p class="p3">
+                        Specialization is a method of production whereby an entity focuses on the production of a limited scope of goods to gain a greater degree of efficiency. Many countries, for example, specialize in producing the goods and services that are native to their part of the world, and they trade for other goods and services.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="vid3">
 
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/HgkWqYDGuFw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
 
-
-
-
-       </div>
-   </section>
-
-       <!--  section4 --------------------------------------------------------------------------------------------------------- -->
-   <section class="section4 text-center">
-
-
-           <div class="container">
-
-          <div class="row">
-                <div class="col-md-4" id="box">  hay  </div>
-                <div class="col-md-4" id="box">   hay </div>
-                <div class="col-md-4" id="box"> hay   </div>
-          </div>
-          </div>
+                </div>
+            </div>
+        </div>
 
 
 
 
 
 
-   </section>
+
+
+
+    </div>
+</section>
+
+<!--  section4 --------------------------------------------------------------------------------------------------------- -->
+<section class="section4 text-center">
+
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-4" id="box"> hay </div>
+            <div class="col-md-4" id="box"> hay </div>
+            <div class="col-md-4" id="box"> hay </div>
+        </div>
+    </div>
+
+
+
+
+
+
+</section>
 <!-------------------------------------------------------------------->
 
 <!---------------------------------------------------------------------->
